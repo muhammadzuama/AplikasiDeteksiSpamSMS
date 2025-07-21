ab86333d87552bd1e4b3822bb5a3584dd5d4e78c
muhammadzuama <muhammadzuama19@gmail.com>
2025-07-22 01:10:36 +0700

    perbaikan kecil

--- deploy/app.py
+++ deploy/app.py
@@ -5,6 +5,10 @@ import re
 from collections import defaultdict
 import pandas as pd
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # === Model class ===
 class SpamDetection(nn.Module):
@@ -89,4 +93,4 @@ def home():
     return jsonify({"message": "API Deteksi Spam aktif. Gunakan endpoint POST /predict"}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
